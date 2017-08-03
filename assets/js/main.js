7da245f0d7a730718c5284a849f5c806c24744d8
Haries Ramdhani <hydrolizedmaltose@gmail.com>
2017-08-03 20:30:28 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$('#register').click(function(event) {
+    $('.registForm').css('visibility', 'visible');
+    console.log("Test");
+});
+
+$('#closeButton').click(function(event) {
+    $('.registForm').css('visibility', 'hidden');
+});
