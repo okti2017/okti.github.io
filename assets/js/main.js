1a5d1adf761ea7562c562d9e5fd22a556e5077f3
Haries Ramdhani <hydrolizedmaltose@gmail.com>
2017-08-03 23:28:06 +0300

    Newest state #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+$('#home').children().hide();
+$('#home').children().show(1000);
+$('#guidelines').children().hide();
+$('#about').children().hide();
+$('#contact').children().hide();
+console.log("hiiii");
+
 $('#register').click(function(event) {
     $('.registForm').css('visibility', 'visible');
     console.log("Test");
@@ -5,4 +12,40 @@ $('#register').click(function(event) {
 
 $('#closeButton').click(function(event) {
     $('.registForm').css('visibility', 'hidden');
+});
+
+$('#panduan').click(function(event) {
+    $('#home').children().hide(1000);
+    $('#about').children().hide(1000);
+    $('#guidelines').children().show(1000);
+});
+
+$('#hubungi').click(function(event) {
+    $('#home').children().hide(1000);
+    $('#about').children().hide(1000);
+    $('#guidelines').children().hide(1000);
+    $('#contact').children().show(1000);
+});
+
+$('.aboutButton').click(function(event) {
+    $('#home').children().hide(1000);
+    $('#about').children().hide(1000);
+    $('#contact').children().hide(1000);
+    $('#guidelines').children().show(1000);
+    console.log("Test");
+});
+
+$('#tentang').click(function(event) {
+    $('#home').children().hide(1000);
+    $('#guidelines').children().hide(1000);
+    $('#contact').children().hide(1000);
+    $('#about').children().show(1000);
+    console.log("Test");
+});
+
+$('#beranda').click(function(event) {
+    $('#guidelines').children().hide(1000);
+    $('#about').children().hide(1000);
+    $('#contact').children().hide(1000);
+    $('#home').children().show(1000);
 });
