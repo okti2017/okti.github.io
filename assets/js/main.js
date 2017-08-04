d873fbc53a9a5865b6a1c4e21910e01b3f22b3bb
Haries Ramdhani <hydrolizedmaltose@gmail.com>
2017-08-04 08:32:20 +0300

    Added videos

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@ $('#home').children().fadeIn(1000);
 $('#guidelines').children().hide();
 $('#about').children().hide();
 $('#contact').children().hide();
-console.log("hiiii");
+$('#indoVideo').hide();
+$('#turkeyVideo').hide();
+$('#indoTurkey').children().hide();
 
 $('#register').click(function(event) {
     $('.registForm').css('visibility', 'visible');
@@ -18,6 +20,7 @@ $('#panduan').click(function(event) {
     $('#home').children().hide();
     $('#about').children().hide();
     $('#contact').children().hide();
+    $('#indoTurkey').children().hide();
     $("#guidelines").children().fadeIn(1500);
     // $('#guidelines').children().fadeIn(1500);
 });
@@ -26,28 +29,49 @@ $('#hubungi').click(function(event) {
     $('#home').children().hide();
     $('#about').children().hide();
     $('#guidelines').children().hide();
+    $('#indoTurkey').children().hide();
     $('#contact').children().fadeIn(1500);
+});
+
+$('#indoTurkeyNav').click(function(event) {
+    $('#home').children().hide();
+    $('#about').children().hide();
+    $('#guidelines').children().hide();
+    $('#contact').children().hide();
+    $('#indoTurkey').children().fadeIn(1500);
 });
 
 $('.aboutButton').click(function(event) {
     $('#home').children().hide();
     $('#about').children().hide();
     $('#contact').children().hide();
+    $('#indoTurkey').children().hide();
     $('#guidelines').children().fadeIn(1500);
-    console.log("Test");
 });
 
 $('#tentang').click(function(event) {
     $('#home').children().hide();
     $('#guidelines').children().hide();
     $('#contact').children().hide();
+    $('#indoTurkey').children().hide();
     $('#about').children().fadeIn(1500);
-    console.log("Test");
 });
 
 $('#beranda').click(function(event) {
     $('#guidelines').children().hide();
     $('#about').children().hide();
     $('#contact').children().hide();
+    $('#indoTurkey').children().hide();
     $('#home').children().fadeIn(1500);
+});
+
+$('#indoSign').click(function(event) {
+    $('#turkeyVideo').hide();
+    $('#indoVideo').fadeIn(500);
+    console.log("TESTSTSTSTS");
+});
+
+$('#turkeySign').click(function(event) {
+    $('#indoVideo').hide();
+    $('#turkeyVideo').fadeIn(500);
 });
